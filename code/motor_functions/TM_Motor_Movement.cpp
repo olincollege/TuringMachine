6f5d0e55b5b8f630443e499f1e42d6462b61423a
Mark Belanger <mbelanger@olin.edu>
2023-11-19 20:17:48 -0500

    Remove solenoid and horizontal draw functions

--- code/motor_functions/TM_Motor_Movement.cpp
+++ code/motor_functions/TM_Motor_Movement.cpp
@@ -6,8 +6,9 @@
 TM_Motor_Movement::TM_Motor_Movement(
     uint8_t film_dc_pin,
     uint8_t stepper_pin,
-    uint8_t solenoid_pin,
     uint8_t eraser_pin,
+    uint8_t eraser_min,
+    uint8_t eraser_max,
     uint8_t stepper_total_steps,
     uint8_t stepper_move_steps,
     uint8_t stepper_speed,
@@ -27,8 +28,9 @@ TM_Motor_Movement::TM_Motor_Movement(
   stepperM_move_steps(stepper_move_steps),
   stepperM_speed(stepper_speed),
 
-  solenoidM_pin(solenoid_pin),
   eraserM_pin(eraser_pin),
+  eraserM_min(eraser_min),
+  eraserM_max(eraser_max),
 
   draw_actuation_servoM_pin(draw_actuation_servo_pin),
   draw_control_servoM_pin(draw_control_servo_pin),
@@ -45,7 +47,6 @@ TM_Motor_Movement::TM_Motor_Movement(
     AFMS = Adafruit_MotorShield();
     fineFilmControl = AFMS.getStepper(stepperM_total_steps, film_dcM_pin);
     coarseFilmControl = AFMS.getMotor(film_dcM_pin);
-    eraserActuation = AFMS.getMotor(solenoidM_pin);
     eraserControl = AFMS.getMotor(eraserM_pin);
     drawingActuation.attach(draw_actuation_servoM_pin);
     drawingControl.attach(draw_control_servoM_pin);
@@ -60,9 +61,8 @@ TM_Motor_Movement::TM_Motor_Movement(
     coarseFilmControl->run(FORWARD);                    // Set DC motors to run forward
     fineFilmControl->step(100, FORWARD, MICROSTEP);     //rotate stepper half a turn
     coarseFilmControl->setSpeed(0);                     // Stop motor
-
-
   }
+
   void TM_Motor_Movement::moveFilmBackward() {
     fineFilmControl->setSpeed(stepperM_speed);                      // Set stepper speed to 10 rpm
     coarseFilmControl->setSpeed(dcM_film_speed);              // Set motor speed and start motor
@@ -70,31 +70,36 @@ TM_Motor_Movement::TM_Motor_Movement(
     fineFilmControl->step(100, BACKWARD, MICROSTEP);     //rotate stepper half a turn
     coarseFilmControl->setSpeed(0); 
   }
+
   void TM_Motor_Movement::markerDown() {
     drawingActuation.write(marker_down_posi);
   }
+
   void TM_Motor_Movement::markerUp() {
     drawingActuation.write(marker_up_posi);
   }
+
   void TM_Motor_Movement::eraserDown() {
 
   }
+
   void TM_Motor_Movement::eraserUp() {
 
   }
+
   void TM_Motor_Movement::headPositiveVerticalLine() {
     drawingControl.write(head_max);
   }
+
   void TM_Motor_Movement::headNegativeVerticalLine() {
     drawingControl.write(head_min);
   }
-  
-  // void TM_Motor_Movement::drawForwardHorizontalLine() {
 
-  // }
-  // void TM_Motor_Movement::drawBackwardHorizontalLine() {
-
-  // }
+  void TM_Motor_Movement::erase() {
+    eraserDown();
+    //STUFF
+    eraserUp();
+  }
 
   void TM_Motor_Movement::drawOne() {
     markerDown();
@@ -103,6 +108,7 @@ TM_Motor_Movement::TM_Motor_Movement(
     headNegativeVerticalLine();
 
   }
+
   void TM_Motor_Movement::drawZero() {
     markerDown();
     headPositiveVerticalLine();
@@ -110,24 +116,24 @@ TM_Motor_Movement::TM_Motor_Movement(
     headNegativeVerticalLine();
     moveFilmBackward();
   }
-  void TM_Motor_Movement::erase() {
-    eraserDown();
-    //STUFF
-    eraserUp();
-  }
-  void TM_Motor_Movement:: {
+
+  void TM_Motor_Movement::drawX() {
 
   }
-  void TM_Motor_Movement:: {
+
+  void TM_Motor_Movement::drawY() {
 
   }
-  void TM_Motor_Movement:: {
+
+  void TM_Motor_Movement::drawBlank() {
 
   }
-  void TM_Motor_Movement:: {
+
+  void TM_Motor_Movement::drawHash() {
 
   }
-  void TM_Motor_Movement:: {
+
+  void TM_Motor_Movement::drawArrow() {
 
   }
 
